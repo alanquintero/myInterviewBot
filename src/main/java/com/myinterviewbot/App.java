cf08374850c17af687d5c0adacec365327b931c9
Alan Quintero <alan_q_b@hotmail.com>
2025-10-22 23:05:26 -0600

    Initial Spring Boot setup for myInterviewBot

---
+++ src/main/java/com/myinterviewbot/App.java
@@ -0,0 +1,13 @@
+package com.myinterviewbot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
