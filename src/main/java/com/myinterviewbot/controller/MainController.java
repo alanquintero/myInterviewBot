527022e185bb8c2e721b307dffbf78460780c15a
Alan Quintero <alan_q_b@hotmail.com>
2025-10-29 23:16:08 -0600

    Creating fragment and templated using thymeleaf

---
+++ src/main/java/com/myinterviewbot/controller/MainController.java
@@ -0,0 +1,36 @@
+package com.myinterviewbot.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
+    @GetMapping("/")
+    public String showMainPage(Model model) {
+        model.addAttribute("pageTitle", "Behavioral Interview");
+        model.addAttribute("currentPage", "behavioralPage");
+        return "layout";
+    }
+
+    @GetMapping("/interview/behavioral")
+    public String behavioral(Model model) {
+        LOGGER.info("/interview/behavioral");
+        model.addAttribute("pageTitle", "Behavioral Interview");
+        model.addAttribute("content", "behavioralPage :: content");
+        return "layout";
+    }
+
+    @GetMapping("/interview/resume")
+    public String resume(Model model) {
+        LOGGER.info("/interview/resume");
+        model.addAttribute("pageTitle", "Resume Interview");
+        model.addAttribute("content", "resumePage :: content");
+        return "layout";
+    }
+}
